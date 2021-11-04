75e02a81a0e43746103509844ef7ef064a87546c
Victor Frette <victorafrette@gmail.com>
2021-11-03 22:24:36 -0300

    hola

--- proyecto.js
+++ proyecto.js
@@ -14,14 +14,14 @@ class mueble {
     this.stock = stock;
   }
   //método para mostrar el stock del objeto
-  getStock = function () {
+  /*getStock = function () {
     if (this.stock > 0) {
       this.stock--;
       return true;
     } else {
       return false;
     }
-  };
+  }*/
 
   mostrarPrec() {
     alert(this.nombre + " cuesta $ " + this.precio + " con IVA incluido");
@@ -34,7 +34,7 @@ class mueble {
 
 //Se agregan los datos de tres tipos de productos
 let mueble1 = new mueble(1, "ALACENA", 9000, 0, 20, "S");
-let mueble2 = new mueble(2, "RACKTV", 1000, 0, 20, "N");
+let mueble2 = new mueble(2, "RACKTV", 10000, 0, 20, "N");
 let mueble3 = new mueble(3, "PLACARD", 20000, 0, 20, "S");
 
 //array de muebles disponibles
@@ -62,8 +62,55 @@ function armarListaProd() {
     bloque.appendChild(texto1);
     bloque.appendChild(texto2);
     bloque.appendChild(boton);
-    console.log(boton);
+    //console.log(boton);
   }
+  //determinamos si hay productos en la sesion
+  productoAlmacenado();
+}
+
+//funcion para determinar datos cargados en el sesión
+function productoAlmacenado() {
+  if (sessionStorage.productosLocal == undefined) {
+    actualizarProductos();
+  } else {
+    /* Si existe un carrito, entonces lo sacasmo del Storage*/
+    //Obtenemos el listado de productos almacenado
+    const almacenados = JSON.parse(localStorage.getItem("productosLocal"));
+    //Iteramos almacenados con for...of para transformar todos sus objetos a tipo producto.
+    //for (const objeto of almacenados)
+
+    mueble1.cantidad = almacenados[0].cantidad;
+    mueble2.cantidad = almacenados[1].cantidad;
+    mueble3.cantidad = almacenados[2].cantidad;
+
+    //se crea la lista de productos almacenados
+    for (const cantidades of mueblesDisp) {
+      if ((cantidades.cantidad = !0)) {
+        for (i = 0; i < cantidades.cantidad; i++) {
+          listaProdAgregados(cantidades.nombre);
+        }
+      } else {
+        return;
+      }
+
+      //productos.push(new Producto(objeto));
+      //Ahora tenemos objetos productos y podemos
+    }
+  }
+}
+
+//actualizamos con JSON los datos y guardamos en la sesión
+function actualizarProductos() {
+  const productosLocal = (clave1, valor1) => {
+    sessionStorage.setItem(clave1, valor1);
+  };
+  //Almacenar producto por producto
+  for (const producto of mueblesDisp) {
+    productosLocal(producto.id, JSON.stringify(producto));
+  }
+  productosLocal(accesorio1.nombre, JSON.stringify(aCant[0]));
+  productosLocal(accesorio2.nombre, JSON.stringify(aCant[1]));
+  productosLocal(accesorio3.nombre, JSON.stringify(aCant[2]));
 }
 
 //funcion para armar lista de Productos agregados para compra en el HTML
@@ -74,7 +121,8 @@ function listaProdAgregados(_producto) {
   let boton = document.createElement("a"); //eliminar producto seleccionado
   boton.href = `#`;
   boton.className = `lista__productos--botonEliminar`;
-  if (productos == 1) {
+  let prodAdd = mueble1.cantidad + mueble2.cantidad + mueble3.cantidad;
+  if (prodAdd == 1) {
     agregado.removeChild(sinProducto);
     producto.innerHTML = `${_producto}`;
     boton.innerHTML = `Eliminar`;
@@ -90,46 +138,40 @@ function listaProdAgregados(_producto) {
   let botEliminarProd = document.querySelectorAll(
     ".lista__productos--botonEliminar"
   );
-  //agregar eventos al link Eliminar productos
   for (let boton of botEliminarProd) {
     boton.addEventListener("click", eliminarProd);
   }
 }
 
-//eliminar producto seleccionado
+//eliminar producto seleccionados de la lista
 function eliminarProd(e) {
   let hijo = e.target;
   let padre1 = hijo.parentNode;
   let padre2 = hijo.parentNode.parentNode;
   let vacio = padre2.childElementCount; //número de elementos agregados
-  //console.log(vacio);
-  //console.log(padre1);
-  //console.log(padre2);
 
-  //si en la lista hay más de un producto agregado
+  //si en la lista ya hay un producto agregado
   if (vacio > 1) {
     console.log("Quiere eliminar el producto!");
-    console.log(hijo);
     padre2.removeChild(padre1);
-    //descartar(padre1.firstChild.data);
-    console.log(padre1.firstChild.data);
-    //productos--;
   }
   //si en la lista hay solo un producto agregado
+  //se vuelve a colocar el páarafo "No agregó productos!
+  //al eliminarlo
   else {
     console.log(hijo);
     padre2.removeChild(padre1);
     let agregados = document.getElementById("listaProductosAgregados");
     let sinProducto = document.createElement("p");
     sinProducto.innerHTML = `No agregó productos!`; //al eliminar el unico producto se agrega el texto original
+    sinProducto.id = `sinProducto`;
     agregados.appendChild(sinProducto);
-    //descartar(padre1.firstChild.data);
     console.log(padre1.firstChild.data);
-    //productos--;
   }
+  //función para modificar cantidades
   descartar(padre1.firstChild.data);
+  //se hace el nuevo cálculo y actualiza valor total
   sumar();
-  console.log(vacio);
 }
 
 //clase para accesorios
@@ -159,9 +201,9 @@ function armarListaAcce() {
   let lista = document.querySelector(".seccionAccesorios");
   for (const accesorio of accesoriosDisp) {
     let bloque = document.createElement("div");
-    let texto1 = document.createElement("h4");
-    let texto2 = document.createElement("p");
-    let boton = document.createElement("a");
+    let texto1 = document.createElement("h4"); //nombre accesorio
+    let texto2 = document.createElement("p"); //precio
+    let boton = document.createElement("a"); //link agregar
     texto1.innerHTML = `${accesorio.nombre}`;
     texto2.innerHTML = `Color: ${accesorio.color} Valor: $ ${accesorio.precio}`;
     boton.innerHTML = `Agregar`;
@@ -172,6 +214,33 @@ function armarListaAcce() {
     bloque.appendChild(texto2);
     bloque.appendChild(boton);
   }
+  //determinamos si hay accesorios en la sesion
+  accesorioAlmacenado();
+}
+//funcion para determinar accesorios cargados en el sesión
+function accesorioAlmacenado() {
+  if (sessionStorage.accesoriosLocal == undefined) {
+    actualizarProductos();
+  } else {
+    /* Si existe, entonces lo sacasmo del Storage*/
+    //Obtenemos el listado de productos almacenado
+    const almacenados = JSON.parse(localStorage.getItem("productosLocal"));
+
+    aCant[0] = almacenados[4];
+    aCant[1] = almacenados[5];
+    aCant[2] = almacenados[6];
+    //productos.push(new Producto(objeto));
+    //se crea la lista de accesorios almacenados
+    for (index = 0; index < 3; index++) {
+      if ((aCant[index] = !0)) {
+        for (i = 0; i < aCant[index]; i++);
+        listaAcceAgregados(accesoriosDisp[index].nombre);
+      } else {
+        return;
+      }
+    }
+  }
+  //for (i= cantidades of aCant) {
 }
 
 //funcion para armar lista de accesorios agregados para compra en HTML
@@ -179,10 +248,11 @@ function listaAcceAgregados(_tipoAcce) {
   let agregado = document.getElementById("listaAccesoriosAgregados");
   let sinAccesorio = document.getElementById("sinAccesorio");
   let accesorio = document.createElement("p");
-  let boton = document.createElement("a");
+  let boton = document.createElement("a"); //eliminar accesorio seleccionado
   boton.href = `#`;
   boton.className = `lista__accesorios--botonEliminar`;
-  if (cantiAcces == 1) {
+  let accesAdd = aCant[0] + aCant[1] + aCant[2];
+  if (accesAdd == 1) {
     agregado.removeChild(sinAccesorio);
     accesorio.innerHTML = `${_tipoAcce}`;
     boton.innerHTML = `Eliminar`;
@@ -224,6 +294,7 @@ function eliminarAcces(e) {
     let agregados = document.getElementById("listaAccesoriosAgregados");
     let sinAccesorio = document.createElement("p");
     sinAccesorio.innerHTML = `No agregó accesorios!`;
+    sinAccesorio.id = `sinAccesorio`;
     agregados.appendChild(sinAccesorio);
     //cantiAcces--;
   }
@@ -284,17 +355,17 @@ function seleccion(e) {
 
   if (_producto == mueblesDisp[0].nombre) {
     carritoMuebles.push(mueble1); //suma al array
-    productos++;
+    //productos++;
     mueble1.cantidad++;
     //compra.addItem(mueble1);
   } else if (_producto == mueblesDisp[1].nombre) {
     carritoMuebles.push(mueble2); //suma al array
-    productos++;
+    //productos++;
     mueble2.cantidad++;
     //compra.addItem(mueble2);
   } else if (_producto == mueblesDisp[2].nombre) {
     carritoMuebles.push(mueble3); //suma al array
-    productos++;
+    //productos++;
     mueble3.cantidad++;
     //compra.addItem(mueble3);
   } else {
@@ -313,15 +384,15 @@ function seleccion(e) {
 //función para descartar el producto
 function descartar(_producto) {
   if (_producto == mueblesDisp[0].nombre) {
-    productos--;
+    //productos--;
     mueble1.cantidad--;
     //compra.addItem(mueble1);
   } else if (_producto == mueblesDisp[1].nombre) {
-    productos--;
+    //productos--;
     mueble2.cantidad--;
     //compra.addItem(mueble2);
   } else if (_producto == mueblesDisp[2].nombre) {
-    productos--;
+    //productos--;
     mueble3.cantidad--;
     //compra.addItem(mueble3);
   } else {
@@ -329,32 +400,6 @@ function descartar(_producto) {
     console.log("Selección invalida");
   }
 }
-
-/*Función para validar y mostrar el valor del producto
-function validacion(_prodIngresa) {
-  //*Recibe la variable global producto
-  if (_prodIngresa == "ALACENA") {
-    //Se utilizan los métodos de clase "mueble"
-    mueble1.mostrarPrec();
-    mueble1.mostrarAcc();
-    //cantidad(1); //se llama a la función para definir cantidades
-    listaProdAgregados(_prodIngresa); //función para mostrar en HTML
-  } else if (_prodIngresa == "RACKTV") {
-    //Se utilizan los métodos de clase "mueble"
-    mueble2.mostrarPrec();
-    mueble2.mostrarAcc();
-    //cantidad(1); //se llama a la función para definir cantidades
-    listaProdAgregados(_prodIngresa);
-  } else if (_prodIngresa == "PLACARD") {
-    //Se utilizan los métodos de clase "mueble"
-    mueble3.mostrarPrec();
-    mueble3.mostrarAcc();
-    //cantidad(1); //se llama a la función para definir cantidades
-    listaProdAgregados(_prodIngresa);
-  } else {
-    return (valido = ""); //*Retorna la variable global valido vacía.
-  }
-}*/
 
 //Función para ingresar accesorios
 function definAcc(e) {
@@ -369,19 +414,19 @@ function definAcc(e) {
     //console.log("Ha seleccionado el accesorio A");
     aCant[0] = aCant[0] + 1; //suma cantidad
     accesoriosAgregados.push(accesorio1); //suma al array
-    cantiAcces++;
+    //cantiAcces++;
     //agregaAcc = prompt("Desea agregar otro accesorio? Escriba S ó N");
   } else if (_tipoAcce == accesoriosDisp[1].nombre) {
     //console.log("Ha seleccionado el accesorio B");
     aCant[1] = aCant[1] + 1;
     accesoriosAgregados.push(accesorio2);
-    cantiAcces++;
+    //cantiAcces++;
     //agregaAcc = prompt("Desea agregar otro accesorio? Escriba S ó N");
   } else if (_tipoAcce == accesoriosDisp[2].nombre) {
     //console.log("Ha seleccionado el accesorio C");
     aCant[2] = aCant[2] + 1;
     accesoriosAgregados.push(accesorio3);
-    cantiAcces++;
+    //cantiAcces++;
     //agregaAcc = prompt("Desea agregar otro accesorio? Escriba S ó N");
   } else {
     alert("Entrada inválida"); //*El accesorio seleccionado no existe
@@ -403,33 +448,20 @@ function definAcc(e) {
 function descartarAcce(_accesorio) {
   if (_accesorio == accesoriosDisp[0].nombre) {
     cantiAcces--;
-    accesorio1.cantidad--;
+    aCant[0]--;
     //compra.addItem(mueble1);
   } else if (_accesorio == accesoriosDisp[1].nombre) {
     cantiAcces--;
-    accesorio2.cantidad--;
+    aCant[1]--;
     //compra.addItem(mueble2);
   } else if (_accesorio == accesoriosDisp[2].nombre) {
     cantiAcces--;
-    accesorio3.cantidad--;
+    aCant[2]--;
     //compra.addItem(mueble3);
   } else {
     alert("Entrada inválida"); //* no existe
     console.log("Selección invalida");
   }
-}
-
-//Ejecución del programa
-let carrito;
-
-/* Revisamos que no haya un carrito guardado en storage*/
-if (sessionStorage.cart == undefined) {
-  carrito = new compra();
-} else {
-  /* Si existe un carrito, entonces lo sacasmo del Storage*/
-  carritoSS = JSON.parse(sessionStorage.cart);
-  carrito = new Cart(carritoSS.name, carritoSS.items, carritoSS.total);
-  actualizar();
 }
 
 //funcion para calcular el monto total
@@ -446,7 +478,8 @@ function sumar() {
   let totalSuma = sumAccesorios + sumaMuebles;
   console.log(totalSuma);
   total.textContent = `Total: ${totalSuma}$`;
-  sessionStorage.cart = JSON.stringify(carrito);
+  actualizarProductos();
+  //actualizarAccesorios();
 }
 
 // console.log(carrito.total);
@@ -458,7 +491,3 @@ function sumar() {
 
 //armarListaProd();
 //armarListaAcce();
-//validacion((producto = seleccion()));
-//cantidad(valido);
-//definAcc(cantProd);
-//costoTotal(producto);
